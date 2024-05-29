f7aa7e4f918ee27266e8550856907da55e9fbb38
Lautaro Muller <lautaro8520@gmail.com>
2024-05-29 00:20:33 -0300

    imagen del diagrama

--- Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230841b9d95879e9535a4d2d05e7d729ac20f903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa05351acf256ea2f3b62cee100a6fb7de9d2bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Formularios/obj/Debug/net6.0-windows/Formularios.AssemblyInfo.cs
+++ Formularios/obj/Debug/net6.0-windows/Formularios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4834ad9017ad45a4baf01ad0458e9c53eeb6306f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa05351acf256ea2f3b62cee100a6fb7de9d2bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Formularios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
